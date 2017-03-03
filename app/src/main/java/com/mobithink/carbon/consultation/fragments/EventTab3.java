ed70ce89a3674b35f6233168a125d249f2b765c8
Melanieplaton <mlanieplaton@yahoo.com>
2017-03-03 11:05:04 +0100

    Migration Android studio 2.2.3 (tout cassé ?)

--- app/src/main/java/com/mobithink/carbon/consultation/fragments/EventTab3.java
+++ app/src/main/java/com/mobithink/carbon/consultation/fragments/EventTab3.java
@@ -5,15 +5,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.mobithink.carbon.R;
+import com.mobithink.carbon.database.model.StationDataDTO;
+
+import java.util.ArrayList;
 
 
 public class EventTab3 extends GenericTabFragment {
 
+    ListView stationListView;
+    ArrayList<String> stationNameList;
 
     public EventTab3() {
 
@@ -25,7 +32,9 @@ public class EventTab3 extends GenericTabFragment {
 
         View rootView = inflater.inflate(R.layout.fragment_event_tab3, container, false);
 
-
+        stationListView = (ListView) rootView.findViewById(R.id.station_list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.itemview_station_listview, R.id.stationNameTextView, stationNameList);
+        stationListView.setAdapter(adapter);
 
         return rootView;
     }
@@ -34,6 +43,9 @@ public class EventTab3 extends GenericTabFragment {
     public void onResume() {
         super.onResume();
         getTripDTO();
+        for(StationDataDTO stationDataDTO : getTripDTO().getStationDataDTOList()){
+            stationNameList.add(stationDataDTO.getStationName());
+        }
     }
 
 
